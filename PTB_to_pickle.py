d4e9854e9474bea92b2aa1291172483ebac2ed5c
minjaekim0 <rlaals3620@gmail.com>
2021-03-29 01:06:17 +0900

    Add PTB dataset loading code - Use it to run RNN models - Also upload raw data

---
+++ PTB_to_pickle.py
@@ -0,0 +1,32 @@
+from tqdm import tqdm
+import cupy as cp
+import pickle
+
+
+# Dataset Source: https://deepai.org/dataset/penn-treebank
+
+with open("ptb_sequence.pickle", "wb") as fw:
+    id_to_word = {}
+    word_to_id = {}
+    
+    for purpose in ['train', 'valid', 'test']:
+        sequence = []
+        path = 'ptbdataset/ptb.' + purpose + '.txt'
+
+        with open(path, 'r') as f:
+            words = f.read().replace('\n', '<eos>').strip().split()
+
+            for word in tqdm(words):
+                if word in id_to_word.values():
+                    id_ = word_to_id[word]
+                    sequence.append(id_)
+                else:
+                    id_ = len(word_to_id)
+                    sequence.append(id_)
+                    word_to_id[word] = id_
+                    id_to_word[id_] = word
+        sequence = cp.array(sequence)  
+        pickle.dump(sequence, fw)
+
+    pickle.dump(id_to_word, fw)
+    pickle.dump(word_to_id, fw)
